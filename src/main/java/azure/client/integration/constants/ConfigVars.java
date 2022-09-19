d65a19578748955d4bffd2efc4eb56a8c3769188
Matias Salomone <matisalomone@hotmail.com>
2022-09-19 09:36:58 -0300

    Javadocs comments and english logs

--- src/main/java/azure/client/integration/constants/ConfigVars.java
+++ src/main/java/azure/client/integration/constants/ConfigVars.java
@@ -11,19 +11,25 @@ public class ConfigVars {
   private ConfigVars() {
   }
 
-  /**
+  /*
    * SAS key from Azure Container.
    */
   @Value("${endpoint.azure.container}")
   public String endpointProperty;
 
-  /**
+  /*
+   * Name from the folder in the Azure Container.
+   */
+  @Value("${endpoint.azure.container}")
+  public String saftContainerName ;
+
+  /*
    * SAS key from Azure Container.
    */
   public static final String ENDPOINT_PROP_FOR_MAIN =
       "https://sabrmdev.blob.core.windows.net/saft?sp=racwdli&st=2022-09-15T13:34:40Z&se=2025-09-15T21:34:40Z&spr=https&sv=2021-06-08&sr=c&sig=QurqTW1i7%2BId%2FwD8ZQuIPntdzJ8XGHxnlaJWI7P8qAw%3D";
 
-  /**
+  /*
    * Name from the folder in the Azure Container.
    */
   public static final String CONTAINER = "saft";
